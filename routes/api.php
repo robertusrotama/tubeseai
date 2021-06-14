b1b4da7df795a75ddc97518bd3036b3b872fd381
WAD-IZZA-1202184173 <arianizza16@gmail.com>
2021-06-14 13:21:13 +0700

    Commit

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//DataPegawai
 Route::post('datapegawais', function () {
     return datapegawai::create(request()->all());
 });
@@ -34,15 +35,12 @@ Route::get('datapegawais/{datapegawai}', function($id_pegawai){
     return datapegawai::all();
 });
 
-
-Route::put('datapegawais/{datapegawais:id}', [DataPegawaisController::class,'update']);
-
 Route::delete('datapegawais/{datapegawai}', function (datapegawai $datapegawai){
     $datapegawai->delete();
     return 'Data Berhasil Dihapus';
 }); 
 
-
+//DataPelatihan
 Route::post('datapelatihans', function () {
     return datapelatihan::create(request()->all());
 });
@@ -55,11 +53,51 @@ Route::get('datapelatihans/{datapegawai}', function($id) {
     return datapelatihan::find($id);
 });
 
-Route::put('datapelatihans/{datapelatihans:id}', [DataPelatihansController::class,'update']);
-
 Route::delete('datapelatihans/{datapelatihan}', function (datapelatihan $datapelatihan){
     $datapelatihan->delete();
     return 'Data Berhasil Dihapus';
 }); 
+
+//Presensi
+Route::get('/', [PresensiController::class,'index']);
+Route::post('/presensi', [PresensiController::class,'store']);
+Route::get('/presensi/{Presensi:id}', [PresensiController::class,'show']);
+Route::delete('/presensi/{Presensi:id}', [PresensiController::class,'destroy']);
+Route::put('/presensi/{Presensi:id}', [PresensiController::class,'update']);
+
+//Gaji dan Pangkat
+Route::get('/test', [testcontroller::class,'index']);
+Route::get('/salary', [SalaryController::class,'index']);
+Route::post('/salary', [SalaryController::class,'store']);
+Route::get('/salary/{Salary:id}', [SalaryController::class,'show']);
+Route::delete('/salary/{Salary:id}', [SalaryController::class,'destroy']);
+Route::put('/salary/{Salary:id}', [SalaryController::class,'update']);
+
+Route::get('/career', 'CareerController@index');
+Route::post('/career', 'CareerController@store');
+Route::get('/career/{Career:id}', 'CareerController@show');
+Route::delete('/career/{Career:id}', 'CareerController@destroy');
+Route::put('/career/{Career:id}', 'CareerController@update');
+
+//Pelamar dan Pegawai
+Route::get('/candidate', [CandidateController::class,'index']);
+Route::post('/candidate', [CandidateController::class,'store']);
+Route::get('/candidate/{Candidate:id}', [CandidateController::class,'show']);
+Route::delete('/candidate/{Candidate:id}', [CandidateController::class,'destroy']);
+Route::put('/candidate/{Candidate:id}', [CandidateController::class,'update']);
+
+Route::get('/employee', [EmpolyeeController::class,'index']);
+Route::post('/employee', [EmpolyeeController::class,'store']);
+Route::get('/employee/{Employee:id}', [EmpolyeeController::class,'show']);
+Route::delete('/employee/{Employee:id}', [EmpolyeeController::class,'destroy']);
+Route::put('/employee/{Employee:id}', [EmpolyeeController::class,'update']);
+
+//Cuti
+Route::get('/pengajuancuti', [PengajuanCutiController::class,'index']);
+Route::post('/pengajuancuti', [PengajuanCutiController::class,'store']);
+Route::get('/pengajuancuti/{PengajuanCuti:id}', [PengajuanCutiController::class,'show']);
+Route::delete('/pengajuancuti/{PengajuanCuti:id}', [PengajuanCutiController::class,'destroy']);
+Route::put('/pengajuancuti/{PengajuanCuti:id}', [PengajuanCutiController::class,'update']);
+
 
 
